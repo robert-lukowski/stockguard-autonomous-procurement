f300f40936cc738ed13c26a42932bffe72b46128
Robert A. Lukowski <lukowski.zsg@gmail.com>
2026-08-21 11:53:58 +0200

    feat: validate evidence and emit deterministic rule traces

--- src/server/calle/index.ts
+++ src/server/calle/index.ts
@@ -2,16 +2,20 @@ export { CallEApiAdapter } from "./CallEApiAdapter";
 export { MockCallEAdapter } from "./MockCallEAdapter";
 export type { MockSupplierResult } from "./MockCallEAdapter";
 export { supplierCallResultSchema } from "./resultSchema";
+export { validateSupplierCallResult } from "./validateStructuredResult";
 export {
   CallSafetyError,
   validateCallAuthorization,
 } from "./safety";
 export type {
   CallAuthorization,
+  EvidenceField,
+  FieldEvidence,
   SupplierCallingPort,
   SupplierCallRequest,
   SupplierCallStructuredResult,
   SupplierCallTask,
+  StructuredResultValidation,
   SupportedCallLocale,
   SupportedCallRegion,
 } from "./types";
